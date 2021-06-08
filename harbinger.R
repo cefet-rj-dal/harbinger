1c5def3d3ae64d369a4408facadffd91d6d902b3
RebeccaSalles <rebeccapsalles@gmail.com>
2021-06-08 13:05:49 -0300

    updt_evaluate

--- harbinger.R
+++ harbinger.R
@@ -828,6 +828,41 @@ evtdet.outliers <- function(data,...){
 }
 
 
+hard_metrics <- function(detection,events, beta=1){
+  TP <- sum(detection & events)
+  FP <- sum(detection & !events)
+  FN <- sum(!detection & events)
+  TN <- sum(!detection & !events)
+  
+  confMatrix <- as.table(matrix(c(as.character(TRUE),as.character(FALSE),
+                                  round(TP,2),round(FP,2),
+                                  round(FN,2),round(TN,2)), nrow = 3, ncol = 2, byrow = TRUE,
+                                dimnames = list(c("Detected", "TRUE","FALSE"),
+                                                c("Events", ""))))
+  
+  sensitivity <- TP/(TP+FN)
+  specificity <- TN/(FP+TN)
+  prevalence <- (TP+FN)/(TP+FP+FN+TN)
+  PPV <- (sensitivity * prevalence)/((sensitivity*prevalence) + ((1-specificity)*(1-prevalence)))
+  NPV <- (specificity * (1-prevalence))/(((1-sensitivity)*prevalence) + ((specificity)*(1-prevalence)))
+  detection_rate <- TP/(TP+FP+FN+TN)
+  detection_prevalence <- (TP+FP)/(TP+FP+FN+TN)
+  balanced_accuracy <- (sensitivity+specificity)/2
+  precision <- TP/(TP+FP)
+  recall <- TP/(TP+FN)
+  
+  F1 <- (1+beta^2)*precision*recall/((beta^2 * precision)+recall)
+  
+  h_metrics <- list(TP=TP,FP=FP,FN=FN,TN=TN,confMatrix=confMatrix,
+                    sensitivity=sensitivity, specificity=specificity,
+                    prevalence=prevalence, PPV=PPV, NPV=NPV,
+                    detection_rate=detection_rate, detection_prevalence=detection_prevalence,
+                    balanced_accuracy=balanced_accuracy, precision=precision,
+                    recall=recall, F1=F1)
+  
+  return(h_metrics)
+}
+
 #==== evaluate: Function for evaluating quality of event detection ====
 # input:
 #   events: output from 'evtdet' function regarding a particular times series.
@@ -836,8 +871,8 @@ evtdet.outliers <- function(data,...){
 # output:
 #   calculated metric value.
 evaluate <- function(events, reference, 
-                     metric=c("confusion_matrix","sensitivity","specificity","pos_pred_value","neg_pred_value","precision",
-                              "recall","F1","prevalence","detection_rate","detection_prevalence","balanced_accuracy")){
+                          metric=c("confusion_matrix","sensitivity","specificity","pos_pred_value","neg_pred_value","precision",
+                                   "recall","F1","prevalence","detection_rate","detection_prevalence","balanced_accuracy"), beta=1){
   #browser()
   if(is.null(events) | is.null(events$time)) stop("No detected events were provided for evaluation",call. = FALSE)
   
@@ -847,26 +882,27 @@ evaluate <- function(events, reference,
   reference_vec <- as.logical(reference$event)
   detected_vec <- as.logical(detected$event)
   
+  hardMetrics <- hard_metrics(detected_vec, reference_vec, beta=beta)
+  
   metric <- match.arg(metric)
   
-  func <-
-  switch(metric,
-    "confusion_matrix" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$table,
-    "sensitivity" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["Sensitivity"],
-    "specificity" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["Specificity"],
-    "pos_pred_value" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["Pos Pred Value"],
-    "neg_pred_value" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["Neg Pred Value"],
-    "precision" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["Precision"],
-    "recall" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["Recall"],
-    "F1" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["F1"],
-    "prevalence" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["Prevalence"],
-    "detection_rate" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["Detection Rate"],
-    "detection_prevalence" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["Detection Prevalence"],
-    "balanced_accuracy" = function(data, reference, ...) caret::confusionMatrix(factor(data),factor(reference),...)$byClass["Balanced Accuracy"]
-  )
+  metric_value <- switch(metric,
+                         "confusion_matrix" = hardMetrics$confMatrix,
+                         "sensitivity" = hardMetrics$sensitivity,
+                         "specificity" = hardMetrics$specificity,
+                         "pos_pred_value" = hardMetrics$PPV,
+                         "neg_pred_value" = hardMetrics$NPV,
+                         "precision" = hardMetrics$precision,
+                         "recall" = hardMetrics$recall,
+                         "F1" = hardMetrics$F1,
+                         "prevalence" = hardMetrics$prevalence,
+                         "detection_rate" = hardMetrics$detection_rate,
+                         "detection_prevalence" = hardMetrics$detection_prevalence,
+                         "balanced_accuracy" = hardMetrics$balanced_accuracy)
   
-  do.call(func,c(list(detected_vec),list(reference_vec)))
+  return(metric_value)
 }
+
 
 #==== plot: Function for plotting event detection ====
 # input:
