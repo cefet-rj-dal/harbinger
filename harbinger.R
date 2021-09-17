345316566260e6ecedfe4cf97f49731616a07c8c
Rebecca Pontes Salles <rebeccapsalles@gmail.com>
2021-09-17 19:24:46 -0300

    upt_HSM

--- harbinger.R
+++ harbinger.R
@@ -906,6 +906,133 @@ evaluate <- function(events, reference,
 }
 
 
+soft_scores <- function(detection, events, k=15){
+  
+  E <- which(events)
+  m <- length(E)
+  
+  D <- which(detection)
+  n <- length(D)
+  
+  mu <- function(j,i,E,D,k) max(min( (D[i]-(E[j]-k))/k, ((E[j]+k)-D[i])/k ), 0)
+  
+  Mu <- matrix(NA,nrow = n, ncol = m)
+  for(j in 1:m) for(i in 1:n) Mu[i,j] <- mu(j,i,E,D,k)
+  
+  E_d <- list()
+  for(i in 1:n) E_d[[i]] <- which(Mu[i,] == max(Mu[i,]))
+  
+  D_e <- list()
+  for(j in 1:m) D_e[[j]] <- which(sapply(1:n, function(i) j %in% E_d[[i]] & Mu[i,j] > 0)) 
+  
+  d_e <- c()
+  for(j in 1:m) {
+    if(length(D_e[[j]])==0) d_e[j] <- NA
+    else d_e[j] <- D_e[[j]][which.max(sapply(D_e[[j]], function(i) Mu[i,j]))]
+  }
+  
+  S_e <- c()
+  for(j in 1:m) {
+    if(length(D_e[[j]])==0) S_e[j] <- NA
+    #else S_e[j] <- sum(sapply(D_e[[j]], function(i) Mu[i,j])) / length(D_e[[j]]) #mean
+    else S_e[j] <- max(sapply(D_e[[j]], function(i) Mu[i,j]))  #max
+  }
+  
+  S_d <- c()
+  for(i in 1:n) S_d[i] <- max(S_e[which(d_e == i)], 0)
+  
+  return(S_d)
+}
+
+
+soft_metrics <- function(soft_scores,m,n,t,beta=1){
+  TPs <- sum(soft_scores)
+  FPs <- sum(1-soft_scores)
+  FNs <- m-TPs
+  TNs <- (t-m)-FPs
+  
+  confMatrix <- as.table(matrix(c(as.character(TRUE),as.character(FALSE),
+                         round(TPs,2),round(FPs,2),
+                         round(FNs,2),round(TNs,2)), nrow = 3, ncol = 2, byrow = TRUE,
+                         dimnames = list(c("Detected", "TRUE","FALSE"),
+                                         c("Events", ""))))
+  
+  accuracy <- (TPs+TNs)/(TPs+FPs+FNs+TNs)
+  sensitivity <- TPs/(TPs+FNs)
+  specificity <- TNs/(FPs+TNs)
+  prevalence <- (TPs+FNs)/(TPs+FPs+FNs+TNs)
+  PPV <- (sensitivity * prevalence)/((sensitivity*prevalence) + ((1-specificity)*(1-prevalence)))
+  NPV <- (specificity * (1-prevalence))/(((1-sensitivity)*prevalence) + ((specificity)*(1-prevalence)))
+  detection_rate <- TPs/(TPs+FPs+FNs+TNs)
+  detection_prevalence <- (TPs+FPs)/(TPs+FPs+FNs+TNs)
+  balanced_accuracy <- (sensitivity+specificity)/2
+  precision <- TPs/(TPs+FPs)
+  recall <- TPs/(TPs+FNs)
+  
+  F1 <- (1+beta^2)*precision*recall/((beta^2 * precision)+recall)
+  
+  s_metrics <- list(TPs=TPs,FPs=FPs,FNs=FNs,TNs=TNs,confMatrix=confMatrix,accuracy=accuracy,
+                    sensitivity=sensitivity, specificity=specificity,
+                    prevalence=prevalence, PPV=PPV, NPV=NPV,
+                    detection_rate=detection_rate, detection_prevalence=detection_prevalence,
+                    balanced_accuracy=balanced_accuracy, precision=precision,
+                    recall=recall, F1=F1)
+  
+  return(s_metrics)
+}
+
+
+
+#==== soft_evaluate: Function for soft evaluating quality of event detection ====
+# input:
+#   events: output from 'evtdet' function regarding a particular times series. 
+#           A data.frame with three variables: time (events time/indexes), serie (corresponding time serie), type (event type)
+#   reference: data.frame of the same length as the time series with two variables: time, event (boolean indicating true events)
+#
+# output:
+#   calculated metric value.
+soft_evaluate <- function(events, reference, k=15,
+                     metric=c("confusion_matrix","accuracy","sensitivity","specificity","pos_pred_value","neg_pred_value","precision",
+                              "recall","F1","prevalence","detection_rate","detection_prevalence","balanced_accuracy"), beta=1){
+  #browser()
+  if(is.null(events) | is.null(events$time)) stop("No detected events were provided for evaluation",call. = FALSE)
+  
+  names(reference) <- c("time","event")
+  detected <- cbind.data.frame(time=reference$time,event=0)
+  detected[detected$time %in% events$time, "event"] <- 1
+  reference_vec <- as.logical(reference$event)
+  detected_vec <- as.logical(detected$event)
+  
+  
+  softScores <- soft_scores(detected_vec, reference_vec, k=k)
+  
+  m <- length(which(reference_vec))
+  n <- length(which(detected_vec))
+  t <- length(reference_vec)
+  
+  softMetrics <- soft_metrics(softScores,m,n,t,beta=beta)
+  
+  metric <- match.arg(metric)
+  
+  metric_value <- switch(metric,
+           "confusion_matrix" = softMetrics$confMatrix,
+           "accuracy" = softMetrics$accuracy,
+           "sensitivity" = softMetrics$sensitivity,
+           "specificity" = softMetrics$specificity,
+           "pos_pred_value" = softMetrics$PPV,
+           "neg_pred_value" = softMetrics$NPV,
+           "precision" = softMetrics$precision,
+           "recall" = softMetrics$recall,
+           "F1" = softMetrics$F1,
+           "prevalence" = softMetrics$prevalence,
+           "detection_rate" = softMetrics$detection_rate,
+           "detection_prevalence" = softMetrics$detection_prevalence,
+           "balanced_accuracy" = softMetrics$balanced_accuracy)
+  
+  return(metric_value)
+}
+
+
 #==== plot: Function for plotting event detection ====
 # input:
 #   data: data.frame with one or more variables (time series) where the first variable refers to time.
