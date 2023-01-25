761386a4aedc19c0429afdc099f5f60b0a8f5745
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2023-01-25 11:33:56 -0300

    Update test.R

--- develop/test.R
+++ develop/test.R
@@ -12,7 +12,20 @@ plot(grf)
 grf <- plot(model, dataset$serie, detection, dataset$event)
 plot(grf)
 
+
+detection <- detect(model, dataset$serie)
+print(detection |> dplyr::filter(event==TRUE))
+evaluation <- evaluate(model, detection$event, dataset$event)
+print(evaluation$confMatrix)
+
+detection <- detect(model, dataset$serie)
 evaluation <- evaluate(model, detection$event, dataset$event, evaluation = soft_evaluation(sw=1))
+print(evaluation$confMatrix)
+
+detection <- detect(model, dataset$serie)
+detection[55, ] <- detection[50, ]
+detection[50, ] <- detection[1, ]
+evaluation <- evaluate(model, detection$event, dataset$event, evaluation = soft_evaluation(sw=15))
 print(evaluation$confMatrix)
 
 
