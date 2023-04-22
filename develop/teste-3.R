853f8b0a060083a18ee3e2c0664b3ffc2fc3ab89
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2023-04-22 10:31:08 -0300

    Update

---
+++ develop/teste-3.R
@@ -0,0 +1,40 @@
+# Harbinger Package
+# version 1.0.9
+
+source("https://raw.githubusercontent.com/cefet-rj-dal/harbinger/master/examples/jupyter_harbinger.R")
+
+#loading Harbinger
+load_harbinger()
+
+#loading the example database
+data(har_examples)
+
+#Using the time series 16
+dataset <- har_examples[[16]]
+head(dataset)
+
+#ploting serie #16
+
+plot(x = 1:length(dataset$serie), y = dataset$serie)
+lines(x = 1:length(dataset$serie), y = dataset$serie)
+
+# establishing garch method
+model <- har_kmeans(3)
+
+# fitting the model
+model <- fit(model, dataset$serie)
+
+# making detections using fbiad
+detection <- detect(model, dataset$serie)
+
+# filtering detected events
+print(detection |> dplyr::filter(event==TRUE))
+
+# evaluating the detections
+evaluation <- evaluate(model, detection$event, dataset$event)
+print(evaluation$confMatrix)
+
+# ploting the results
+grf <- plot.harbinger(model, dataset$serie, detection, dataset$event)
+plot(grf)
+
