606d7d4b49186873ca907b02eea4b478ee9e2d77
janiosl <janio.oriente@gmail.com>
2023-05-07 15:17:04 -0300

    Update

--- develop/nexus_experiments.R
+++ develop/nexus_experiments.R
@@ -1,23 +1,26 @@
 #############################################################################
 ## Integration between Nexus + Harbinger + DALEvents
 #############################################################################
-
-# Install and load Harbinger and Nexus ------------------------------------
 # install.packages("devtools")
-#devtools::install_github("cefet-rj-dal/harbinger")
+library(devtools)
 
+# Install and load Harbinger, Nexus and DalEvents -----------------------------
+devtools::install_github("cefet-rj-dal/harbinger",
+                         force = TRUE, dep=FALSE, upgrade="never")
+devtools::install_github("cefet-rj-dal/event_datasets",
+                         force = TRUE)
+
+# Load packages
 library(harbinger)
 source("https://raw.githubusercontent.com/cefet-rj-dal/harbinger/master/develop/nexus.R")
-
+#library(nexus)
+library(dalevents)
 
 # Load dataset ------------------------------------------------------------
-devtools::install_github("cefet-rj-dal/event_datasets", force = TRUE)
-library(dalevents)
-data(gecco)
-
 #Selectdesired series and time interval
 #ph variable
 #Interval of a day with anomalies
+data(gecco)
 data <- subset(gecco$gecco[16500:18000,], select = c(ph, event))
 data <- data[1:200,] #Use it only for fast test
 
@@ -27,6 +30,7 @@ data <- subset(fi_br$Commodity, select = c(`Oil Brent`, Event))
 
 #Adjust variabels names
 names(data) <- c("series", "event")
+
 # Run Nexus ---------------------------------------------------------------
 #Prepare data to experiment
 datasource <- nex_simulated_datasource("data", data$series)
@@ -42,10 +46,21 @@ sld_bt <- 1
 #Warmup nexus
 online_detector <- warmup(online_detector)
 
+
 #Sliding batches through series
 while (!is.null(online_detector$datasource)) {
+
   #Online detection
   online_detector <- detect(online_detector)
+
+  #Create merge results
+  if (sld_bt > 1) {
+    x <- online_detector$detection
+    online_detector_merged <- merge(x, online_detector$detection,
+                                    by.x = "idx", by.y = "idx",
+                                    all.x = TRUE)
+  }
+
   print(paste("Current position:", online_detector$detection$idx[sld_bt]))
   print("Results:")
   print(table(online_detector$detection$event))
@@ -86,3 +101,4 @@ grf <- plot.harbinger(online_detector$detector, data$series,
 plot(grf)
 
 View(online_detector$detection)
+View(online_detector_merged)
