48e342c4c9b48b9e4f59fd6a5f2388dda34f97a8
eogasawara <eogasawara@eic.cefet-rj.br>
2023-07-13 07:18:22 -0300

    data documentation

--- R/data.R
+++ R/data.R
@@ -1,5 +1,26 @@
-#' Synthetic data set for time series analysis
-#' Each time series has 101 observations with a single event on it.
+#' Synthetic time series for event detection
+#' @description A list of univariate time series for event detection
+#' \itemize{
+#' \item example1: a single (uncommon value) anomaly example in a stationary time series
+#' \item example2: a single (common value) anomaly example in a stationary time series
+#' \item example3: a single anomaly example in a trend time series
+#' \item example4: a change point in a time series
+#' \item example5: a change point with anomaly (uncommon value) in a time series
+#' \item example6: a change point with anomaly (common value) in a time series
+#' \item example7: a change point with anomaly (uncommon value) in a seasonal time series
+#' \item example8: a change point with anomaly (common value) in a seasonal time series
+#' \item example9: multi behavior time series (1-200: stationary, 201-400: trend, 401-600: structural break; 601-800: heteroscedasticity, 801:1000 random walk)
+#' \item example10: multiple anomalies
+#' \item example11: stationary time series
+#' \item example12: trend time series
+#' \item example13: structural break
+#' \item example14: heterocedasticity
+#' \item example15: motif
+#' \item example16: discord
+#' \item example17: repetitive anomaly
+#' \item example18: repetitive anomaly
+#' }
+#'#'
 #' @docType data
 #' @usage data(har_examples)
 #' @format A list of time series.
@@ -8,11 +29,15 @@
 #' @source \href{https://github.com/cefet-rj-dal/harbinger}{Harbinger package}
 #' @examples
 #' data(har_examples)
-#' serie <- har_examples[[1]]
+#' serie <- har_examples$example1
 "har_examples"
 
-#' Synthetic multivariate data set for time series analysis
-#' Each time series has 101 observations with a single event on it.
+#' Synthetic time series for event detection
+#' @description A list of multivariate time series for event detection
+#' \itemize{
+#' \item example1: an single multivariate anomaly
+#' }
+#'#'
 #' @docType data
 #' @usage data(har_examples_multi)
 #' @format A list of multivariate time series.
@@ -21,5 +46,6 @@
 #' @source \href{https://github.com/cefet-rj-dal/harbinger}{Harbinger package}
 #' @examples
 #' data(har_examples_multi)
-#' serie <- har_examples_multi[[1]]
+#' serie <- har_examples_multi$example1
 "har_examples_multi"
+
