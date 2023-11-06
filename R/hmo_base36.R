c00f149c74226747270fb19dcb04b368fc664d61
eogasawara <eogasawara@eic.cefet-rj.br>
2023-11-06 19:20:02 -0300

    Upgrade

--- R/hmo_base36.R
+++ R/hmo_base36.R
@@ -32,61 +32,67 @@ hmo_base36 <- function(a, w, qtd) {
   obj$a <- a
   obj$w <- w
   obj$qtd <- qtd
+
   class(obj) <- append("hmo_base36", class(obj))
   return(obj)
 }
 
-binning_base36 <- function(v, a) {
-  p <- seq(from = 0, to = 1, by = 1/a)
-  q <- stats::quantile(v, p)
-  qf <- matrix(c(q[1:(length(q)-1)],q[2:(length(q))]), ncol=2)
-  vp <- cut(v, unique(q), FALSE, include.lowest=TRUE)
-  m <- tapply(v, vp, mean)
-  vm <- m[vp]
-  mse <- mean((v - vm)^2, na.rm = TRUE)
-  return (list(binning=m, bins_factor=vp, q=q, qf=qf, bins=vm, mse=mse))
-}
-
-convert_to_base36 <- function(num, nbase) {
-  chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  if(nbase < 2 || nbase > str_length(chars))
-    return("")
-  newNumber <- ""
-  r <- 0
-  while(num >= nbase)
-  {
-    r <- num %% nbase
-    newNumber <- sprintf("%s%s", substr(chars, r+1, r+1), newNumber)
-    num <- as.integer(num / nbase)
-  }
-  newNumber = sprintf("%s%s", substr(chars, num+1, num+1), newNumber)
-  return (newNumber)
-}
-
-convert_to_base36_vec <- function(num, nbase) {
-  n <- length(num)
-  result <- rep("", n)
-  for (i in 1:n) {
-    result[i] <- convert_to_base36(num[i], nbase)
-  }
-  return(result)
-}
-
-norm_base36 <- function (vector, slices)
-{
-  vectorNorm <- (vector - mean(vector, na.rm = T))/stats::sd(vector, na.rm = T)
-  mybin <- binning_base36(vectorNorm, slices)
-  i <- ceiling(log(slices, 36))
-  mycode <- str_pad(convert_to_base36_vec(0:(slices-1), 36), i, pad="0")
-  base36vector <- mycode[mybin$bins_factor]
-  return(base36vector)
-}
-
 #'@importFrom stats na.omit
-#'@import stringr
+#'@importFrom stringr str_length
+#'@importFrom stringr str_pad
 #'@importFrom dplyr group_by
+#'@importFrom dplyr summarise
+#'@importFrom dplyr arrange
+#'@importFrom dplyr select
+#'@importFrom dplyr n
+#'@importFrom dplyr desc
 #'@export
 detect.hmo_base36 <- function(obj, serie, ...) {
+  binning_base36 <- function(v, a) {
+    p <- base::seq(from = 0, to = 1, by = 1/a)
+    q <- stats::quantile(v, p)
+    qf <- base::matrix(c(q[1:(length(q)-1)],q[2:(length(q))]), ncol=2)
+    vp <- base::cut(v, unique(q), FALSE, include.lowest=TRUE)
+    m <- base::tapply(v, vp, mean)
+    vm <- m[vp]
+    mse <- base::mean((v - vm)^2, na.rm = TRUE)
+    return (list(binning=m, bins_factor=vp, q=q, qf=qf, bins=vm, mse=mse))
+  }
+
+  convert_to_base36 <- function(num, nbase) {
+    chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if(nbase < 2 || nbase > stringr::str_length(chars))
+      return("")
+    newNumber <- ""
+    r <- 0
+    while(num >= nbase)
+    {
+      r <- num %% nbase
+      newNumber <- base::sprintf("%s%s", substr(chars, r+1, r+1), newNumber)
+      num <- as.integer(num / nbase)
+    }
+    newNumber = base::sprintf("%s%s", substr(chars, num+1, num+1), newNumber)
+    return (newNumber)
+  }
+
+  convert_to_base36_vec <- function(num, nbase) {
+    n <- length(num)
+    result <- rep("", n)
+    for (i in 1:n) {
+      result[i] <- convert_to_base36(num[i], nbase)
+    }
+    return(result)
+  }
+
+  norm_base36 <- function (vector, slices)
+  {
+    vectorNorm <- (vector - base::mean(vector, na.rm = T))/stats::sd(vector, na.rm = T)
+    mybin <- binning_base36(vectorNorm, slices)
+    i <- base::ceiling(log(slices, 36))
+    mycode <- stringr::str_pad(convert_to_base36_vec(0:(slices-1), 36), i, pad="0")
+    base36vector <- mycode[mybin$bins_factor]
+    return(base36vector)
+  }
   i <- 0
   total_count <- 0
   if(is.null(serie)) stop("No data was provided for computation", call. = FALSE)
@@ -98,25 +104,27 @@ detect.hmo_base36 <- function(obj, serie, ...) {
   tsw <- ts_data(tss, obj$w)
   seq <- apply(tsw, MARGIN = 1, function(x) paste(as.vector(x), collapse=""))
   data <- data.frame(i = 1:nrow(tsw), seq)
-  result <- data |> dplyr::group_by(seq) |> dplyr::summarise(total_count=n()) |> dplyr::filter(total_count >= obj$qtd) |> dplyr::arrange(desc(total_count)) |> dplyr::select(seq)
+  result <- data |> dplyr::group_by(seq) |> dplyr::summarise(total_count=dplyr::n())
+  result <- result[result$total_count >= obj$qtd,]
+  result <- result |> dplyr::arrange(dplyr::desc(total_count)) |> dplyr::select(seq)
   result <- result$seq
-  data <- data |> dplyr::filter(seq %in% result)
+  data <- data[data$seq %in% result,]
 
   motifs <- NULL
   for (j in 1:length(result)) {
-    motif <- data |> dplyr::filter(seq == result[j])
+    motif <- data[data$seq == result[j],]
     pos <- NULL
     for (k in 1:obj$w) {
       pos <- c(pos, motif$i + (k - 1))
     }
-    data <- data |> dplyr::filter(!(i %in% pos))
+    data <- data[!(data$i %in% pos),]
 
     if (nrow(motif) > 0) {
       vec <- motif$i
       svec <- base::split(vec, base::cumsum(c(1, diff(vec) != 1)))
       vec <- base::sapply(svec, min)
 
-      motif <- motif |> dplyr::filter((i %in% vec))
+      motif <- motif[(motif$i %in% vec),]
 
       if (length(vec) >= obj$qtd) {
         motifs <- base::rbind(motifs, motif)
--- R/hmo_sax.R
+++ R/hmo_sax.R
@@ -37,57 +37,63 @@ hmo_sax <- function(a, w, qtd) {
   return(obj)
 }
 
-binning_sax <- function(v, a) {
-  p <- base::seq(from = 0, to = 1, by = 1/a)
-  q <- stats::quantile(v, p)
-  qf <- base::matrix(c(q[1:(length(q)-1)],q[2:(length(q))]), ncol=2)
-  vp <- base::cut(v, unique(q), FALSE, include.lowest=TRUE)
-  m <- base::tapply(v, vp, mean)
-  vm <- m[vp]
-  mse <- base::mean((v - vm)^2, na.rm = TRUE)
-  return (list(binning=m, bins_factor=vp, q=q, qf=qf, bins=vm, mse=mse))
-}
-
-convert_to_sax <- function(num, nbase) {
-  chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  if(nbase < 2 || nbase > stringr::str_length(chars))
-    return("")
-  newNumber <- ""
-  r <- 0
-  while(num >= nbase)
-  {
-    r <- num %% nbase
-    newNumber <- base::sprintf("%s%s", substr(chars, r+1, r+1), newNumber)
-    num <- as.integer(num / nbase)
-  }
-  newNumber = base::sprintf("%s%s", substr(chars, num+1, num+1), newNumber)
-  return (newNumber)
-}
-
-convert_to_sax_vec <- function(num, nbase) {
-  n <- length(num)
-  result <- rep("", n)
-  for (i in 1:n) {
-    result[i] <- convert_to_sax(num[i], nbase)
-  }
-  return(result)
-}
-
-norm_sax <- function (vector, slices)
-{
-  vectorNorm <- (vector - base::mean(vector, na.rm = T))/stats::sd(vector, na.rm = T)
-  mybin <- binning_sax(vectorNorm, slices)
-  i <- base::ceiling(log(slices, 26))
-  mycode <- stringr::str_pad(convert_to_sax_vec(0:(slices-1), 26), i, pad="0")
-  saxvector <- mycode[mybin$bins_factor]
-  return(saxvector)
-}
 
 #'@importFrom stats na.omit
-#'@import stringr
-#'@import dplyr
+#'@importFrom stringr str_length
+#'@importFrom stringr str_pad
+#'@importFrom dplyr group_by
+#'@importFrom dplyr summarise
+#'@importFrom dplyr arrange
+#'@importFrom dplyr select
+#'@importFrom dplyr n
+#'@importFrom dplyr desc
 #'@export
 detect.hmo_sax <- function(obj, serie, ...) {
+  binning_sax <- function(v, a) {
+    p <- base::seq(from = 0, to = 1, by = 1/a)
+    q <- stats::quantile(v, p)
+    qf <- base::matrix(c(q[1:(length(q)-1)],q[2:(length(q))]), ncol=2)
+    vp <- base::cut(v, unique(q), FALSE, include.lowest=TRUE)
+    m <- base::tapply(v, vp, mean)
+    vm <- m[vp]
+    mse <- base::mean((v - vm)^2, na.rm = TRUE)
+    return (list(binning=m, bins_factor=vp, q=q, qf=qf, bins=vm, mse=mse))
+  }
+
+  convert_to_sax <- function(num, nbase) {
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if(nbase < 2 || nbase > stringr::str_length(chars))
+      return("")
+    newNumber <- ""
+    r <- 0
+    while(num >= nbase)
+    {
+      r <- num %% nbase
+      newNumber <- base::sprintf("%s%s", substr(chars, r+1, r+1), newNumber)
+      num <- as.integer(num / nbase)
+    }
+    newNumber = base::sprintf("%s%s", substr(chars, num+1, num+1), newNumber)
+    return (newNumber)
+  }
+
+  convert_to_sax_vec <- function(num, nbase) {
+    n <- length(num)
+    result <- rep("", n)
+    for (i in 1:n) {
+      result[i] <- convert_to_sax(num[i], nbase)
+    }
+    return(result)
+  }
+
+  norm_sax <- function (vector, slices)
+  {
+    vectorNorm <- (vector - base::mean(vector, na.rm = T))/stats::sd(vector, na.rm = T)
+    mybin <- binning_sax(vectorNorm, slices)
+    i <- base::ceiling(log(slices, 26))
+    mycode <- stringr::str_pad(convert_to_sax_vec(0:(slices-1), 26), i, pad="0")
+    saxvector <- mycode[mybin$bins_factor]
+    return(saxvector)
+  }
   i <- 0
   total_count <- 0
   if(is.null(serie)) stop("No data was provided for computation", call. = FALSE)
@@ -98,26 +104,27 @@ detect.hmo_sax <- function(obj, serie, ...) {
   tsw <- daltoolbox::ts_data(tss, obj$w)
   seq <- base::apply(tsw, MARGIN = 1, function(x) paste(as.vector(x), collapse=""))
   data <- data.frame(i = 1:nrow(tsw), seq)
-  result <- data |> dplyr::group_by(seq) |> dplyr::summarise(total_count=n()) |>
-    dplyr::filter(total_count >= obj$qtd) |> dplyr::arrange(desc(total_count)) |> dplyr::select(seq)
+  result <- data |> dplyr::group_by(seq) |> dplyr::summarise(total_count=dplyr::n())
+  result <- result[result$total_count >= obj$qtd,]
+  result <- data |> dplyr::arrange(dplyr::desc(total_count)) |> dplyr::select(seq)
   result <- result$seq
-  data <- data |> dplyr::filter(seq %in% result)
+  data <- data[data$seq %in% result,]
 
   motifs <- NULL
   for (j in 1:length(result)) {
-    motif <- data |> dplyr::filter(seq == result[j])
+    motif <- data[data$seq == result[j],]
     pos <- NULL
     for (k in 1:obj$w) {
       pos <- c(pos, motif$i + (k - 1))
     }
-    data <- data |> dplyr::filter(!(i %in% pos))
+    data <- data[!(data$i %in% pos),]
 
     if (nrow(motif) > 0) {
       vec <- motif$i
       svec <- base::split(vec, base::cumsum(c(1, diff(vec) != 1)))
       vec <- base::sapply(svec, min)
 
-      motif <- motif |> dplyr::filter((i %in% vec))
+      motif <- motif[(motif$i %in% vec),]
 
       if (length(vec) >= obj$qtd) {
         motifs <- base::rbind(motifs, motif)
